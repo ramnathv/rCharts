1265555ba84347753f471c63bb7df6be7f4d4849
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-04-11 16:06:48 -0400

    fix bug in facet

--- inst/rChartApp/server.R
+++ inst/rChartApp/server.R
@@ -3,7 +3,7 @@ shinyServer(function(input, output) {
   output$myChart <- renderChart({
     names(iris) = gsub("\\.", "", names(iris))
     p1 <- rPlot(input$x, input$y, data = iris, color = "Species", 
-      facet = "Species", type = 'point')
+      facet = list(var = "Species", type = 'wrap'), type = 'point')
     p1$addParams(dom = 'myChart')
     return(p1)
   })
