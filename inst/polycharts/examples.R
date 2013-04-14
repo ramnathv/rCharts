fb76aa60be3bf2db39799a4463687fce7c3ac197
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-04-14 10:27:17 -0400

    modify one example to check facet method

--- inst/polycharts/examples.R
+++ inst/polycharts/examples.R
@@ -6,7 +6,8 @@ p1$show(T)
 
 ## Example 2 Facetted Barplot
 hair_eye = as.data.frame(HairEyeColor)
-p2 <- rPlot(Freq ~ Hair | Eye, color = 'Eye', data = hair_eye, type = 'bar')
+p2 <- rPlot(Freq ~ Hair, color = 'Eye', data = hair_eye, type = 'bar')
+p2$facet(var = 'Eye', type = 'wrap')
 p2$show(T)
 
 ## Example 3 Boxplot
