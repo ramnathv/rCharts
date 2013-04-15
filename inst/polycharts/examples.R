7cb08d0b324332979476bcbda84b4a07af9f5718
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-04-14 22:34:35 -0400

    load plyr to fix bug in example run

--- inst/polycharts/examples.R
+++ inst/polycharts/examples.R
@@ -30,7 +30,7 @@ p5$show(T)
 
 
 # Example 6 (NBA Heat Map)
-require(reshape2); require(scales)
+require(reshape2); require(scales); require(plyr)
 nba <- read.csv('http://datasets.flowingdata.com/ppg2008.csv')
 nba.m <- ddply(melt(nba), .(variable), transform, rescale = rescale(value))
 p1 <- rPlot(Name ~ variable, color = 'rescale', data = nba.m, type = 'tile', height = 600)
