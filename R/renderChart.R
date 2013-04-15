df438b937b309e0bd44cf0c1e645cd7f516796c4
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-04-15 10:31:52 -0400

    minor tweaks to doc and add author

--- R/renderChart.R
+++ R/renderChart.R
@@ -1,19 +1,18 @@
 #' renderChart (use with Shiny)
 #' 
-#' Use rNVD3 charts as Shiny output. First, use \code{renderChart} in
-#' \code{server.R} to assign the chart object to an Shiny output. Then create an
-#' chartOutput with the same name in \code{ui.R}. \code{chartOutput} is
-#' currently just an alias for \code{htmlOutput}. 
+#' Use rCharts as Shiny output. First, use \code{renderChart} in \code{server.R} to assign 
+#' the chart object to an Shiny output. Then create an chartOutput with the same name in #'
+#' \code{ui.R}. \code{chartOutput} is currently just an alias for \code{htmlOutput}. 
 #' 
+#' @author Thomas Reinholdsson, Ramnath Vaidyanathan
 #' @param expr An expression that returns a chart object
 #' @param env The environment in which to evaluate \code{expr}.
 #' @param quoted Is expr a quoted expression (with \code{quote()})? This is
-#'   useful if you want to save an expression in a variable.
+#'  useful if you want to save an expression in a variable.
 #'   
 #' @export
 renderChart <- function(expr, env = parent.frame(), quoted = FALSE) {
   func <- shiny::exprToFunction(expr, env, quoted)
-  
   function() {
     rChart_ <- func()
     rChart_$html()
