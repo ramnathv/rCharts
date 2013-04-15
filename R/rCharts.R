79311cd1b7233d21247038560506a289b43a820d
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-04-14 22:29:39 -0400

    replace rCharts with new implementation

--- R/rCharts.R
+++ R/rCharts.R
@@ -1,11 +1,48 @@
-# NOTE: To remove once I confirm that the other implementation works!
-PolyChart2 = setRefClass('PolyChart', list(params = 'list'), methods = list(
-  initialize = function(){
-    params <<- list(dom = basename(tempfile('chart')), width = 700, height = 300,
-      layers = list(), facet = list(), guides = list(), coord = list())
-  },
+rCharts = setRefClass('rCharts', list(params = 'list', lib = 'character'), methods = list(
   addParams = function(...){
     params <<- modifyList(params, list(...))
+  },
+  getPayload = function(chartId){
+    list(chartParams = toJSON(params))
+  },
+  html = function(chartId = NULL){
+    if (!is.null(chartId)) params$dom <<- chartId else chartId <- params$dom
+    params$id <<- params$dom
+    template = read_template(lib, 'layouts', 'chart.html')
+    html = render_template(template, getPayload(chartId))
+    options(RCHART_LIB = lib)
+    return(html)
+  },
+  printChart = function(chartId = NULL){
+    writeLines(.self$html(chartId))
+  },
+  render = function(chartId = NULL){
+    if (!is.null(chartId)) params$dom <<- chartId
+    template = read_template(lib, 'layouts', 'script.html')
+    html = render_template(template, list(params = params, script = .self$html(chartId)))
+  },
+  save = function(destfile = 'index.html'){
+    writeLines(.self$render(), destfile)
+  },
+  show = function(static = !("shiny" %in% rownames(installed.packages()))){
+    if (static){
+      tf <- tempfile(fileext = 'html');
+      writeLines(.self$render(), tf)
+      system(sprintf("open %s", tf))
+    } else {
+      shiny_copy = .self$copy()
+      shiny_copy$params$dom = 'show'
+      assign(".rChart_object", shiny_copy, envir = .GlobalEnv) 
+      shiny::runApp(file.path(system.file(package = "rCharts"), "shiny"))
+    }
+  }
+))
+
+PolyChart = setRefClass('PolyChart', contains = 'rCharts', methods = list(
+  initialize = function(){
+    lib <<- 'polycharts'
+    params <<- list(dom = basename(tempfile('chart')), width = 700, height = 300,
+      layers = list(), facet = list(), guides = list(), coord = list())
   },
   layer = function(...){
     len = length(params$layers)
@@ -24,34 +61,5 @@ PolyChart2 = setRefClass('PolyChart', list(params = 'list'), methods = list(
   },
   coord = function(...){
     params$coord <<- modifyList(params$coord, list(...))
-  },
-  html = function(chartId = NULL){
-    if (!is.null(chartId)) params$dom <<- chartId
-    template = read_template('polycharts', 'layouts', 'chart.html')
-    html = render_template(template, list(chartParams = toJSON(params)))
-    return(html)
-  },
-  printChart = function(chartId = NULL){
-    writeLines(.self$html(chartId))
-  },
-  render = function(chartId = NULL){
-    if (!is.null(chartId)) params$dom <<- chartId
-    template = read_template('polycharts', 'layouts', 'script.html')
-    html = render_template(template, list(params = params, script = .self$html(chartId)))
-  },
-  save = function(destfile = 'index.html'){
-    writeLines(.self$render(), destfile)
-  },
-  show = function(static = !("shiny" %in% rownames(installed.packages()))){
-    if (static){
-      tf <- tempfile(fileext = 'html');
-      writeLines(.self$render(), tf)
-      system(sprintf("open %s", tf))
-    } else {
-      shiny_copy = .self$copy()
-      shiny_copy$params$dom = 'show'
-      assign(".rChart_object", shiny_copy, envir = .GlobalEnv) 
-      shiny::runApp(file.path(system.file(package = "rCharts"), "shiny"))
-    }
   }
 ))
