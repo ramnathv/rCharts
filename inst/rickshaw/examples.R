c3fb86a9bb4df3f78929125ae675c3d6d8536dd0
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-04-18 15:35:41 -0400

    edit Rickshaw layouts and add examples.R

--- inst/rickshaw/examples.R
+++ inst/rickshaw/examples.R
@@ -3,3 +3,12 @@ p1 <- Rickshaw$new()
 p1$layer(~ cyl, group = 'am', data = mtcars, type = 'bar')
 
 # Example 2
+require(RColorBrewer)
+data(economics, package = 'ggplot2')
+datm = reshape2::melt(
+  economics[,c('date', 'psavert', 'uempmed')],
+  id = 'date'
+)
+p2 <- Rickshaw$new()
+p2$layer(value ~ date, group = 'variable', data = datm, type = 'line', 
+  colors = c("darkred", "darkslategrey"))
