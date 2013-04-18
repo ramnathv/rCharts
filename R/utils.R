b131b5436f8b9272b459653e69ecd9ab77ae51c4
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-04-18 15:35:18 -0400

    add merge_list to utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,15 @@
+merge_list <- function (x, y, ...){
+  if (length(x) == 0) 
+    return(y)
+  if (length(y) == 0) 
+    return(x)
+  i = match(names(y), names(x))
+  i = is.na(i)
+  if (any(i)) 
+    x[names(y)[which(i)]] = y[which(i)]
+  return(x)
+}
+
 setSpec = function(spec, ... , replace = F){
   if (replace){
     list(...)
