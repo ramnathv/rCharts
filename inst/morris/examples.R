078ae662b1da388e43dd3a9d3c43b720b65eb338
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-04-19 21:54:50 -0400

    update examples for morrisjs and rickshaw

--- inst/morris/examples.R
+++ inst/morris/examples.R
@@ -1,22 +1,27 @@
+require(rCharts)
 haireye = as.data.frame(HairEyeColor)
 
-## Example 1
+## Simple Bar Chart
 dat = subset(haireye, Sex == "Female" & Eye == "Blue")
-p1 <- Morris$new()
-p1$layer(x = 'Hair', y = list('Freq'), data = dat, type = 'Bar', labels = list("Count"))
+p1 <- mPlot(x = 'Hair', y = list('Freq'), data = dat, type = 'Bar', labels = list("Count"))
 p1$show(T)
 
-## Example 2
+### Check if hideHover = "auto" switches hover off when not on chart
+p1$set(hideHover = "auto")
 
+## Multi Bar Chart
 dat = subset(haireye, Sex == "Female")
-p2 <- Morris$new()
-p2$layer(x = "Eye", y = "Freq", group = "Hair", data = dat, type = 'Bar', labels = 1:4)
+p2 <- mPlot(Freq ~ Eye, group = "Hair", data = dat, type = "Bar", labels = 1:4)
+p2$show(T)
 
-## Example 3
+## Line Chart
 data(economics, package = 'ggplot2')
 dat = transform(economics, date = as.character(date))
-p3 <- Morris$new()
-p3$layer(x = "date", y = list("psavert", "uempmed"), data = dat, type = 'Line',
-  labels = list('Savings Rate', 'Median Duration of Unemployment'))
-p3$addParams(pointSize = 0)
+p3 <- mPlot(x = "date", y = list("psavert", "uempmed"), data = dat, type = 'Line',
+ pointSize = 0, lineWidth = 1)
+
+
+## Area Chart
+p3$set(type = 'Area')
+p3$show(T)
 
--- inst/rickshaw/examples.R
+++ inst/rickshaw/examples.R
@@ -12,3 +12,12 @@ datm = reshape2::melt(
 p2 <- Rickshaw$new()
 p2$layer(value ~ date, group = 'variable', data = datm, type = 'line', 
   colors = c("darkred", "darkslategrey"))
+
+
+p3 <- Rickshaw$new()
+p3$layer(Employed ~ Year, data = longley, type = 'line', colors = c('darkred'))
+
+usp = reshape2::melt(USPersonalExpenditure)
+p4 <- Rickshaw$new()
+p4$layer(value ~ Var2, group = 'Var1', data = usp, type = 'area')
+p4$show(T)
