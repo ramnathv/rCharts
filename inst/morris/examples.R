90a7c79b0379a4f7625db20e0ce8a96a7fabf97e
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-04-22 21:59:46 -0400

    remove show(T) as it has been made default.

--- inst/morris/examples.R
+++ inst/morris/examples.R
@@ -4,24 +4,26 @@ haireye = as.data.frame(HairEyeColor)
 ## Simple Bar Chart
 dat = subset(haireye, Sex == "Female" & Eye == "Blue")
 p1 <- mPlot(x = 'Hair', y = list('Freq'), data = dat, type = 'Bar', labels = list("Count"))
-p1$show(T)
+p1
 
 ### Check if hideHover = "auto" switches hover off when not on chart
 p1$set(hideHover = "auto")
+p1
 
 ## Multi Bar Chart
 dat = subset(haireye, Sex == "Female")
 p2 <- mPlot(Freq ~ Eye, group = "Hair", data = dat, type = "Bar", labels = 1:4)
-p2$show(T)
+p2
 
 ## Line Chart
 data(economics, package = 'ggplot2')
 dat = transform(economics, date = as.character(date))
 p3 <- mPlot(x = "date", y = list("psavert", "uempmed"), data = dat, type = 'Line',
  pointSize = 0, lineWidth = 1)
+p3
 
 
 ## Area Chart
 p3$set(type = 'Area')
-p3$show(T)
+p3
 
