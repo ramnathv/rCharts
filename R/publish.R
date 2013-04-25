33bb29fd47ff85ebd5dd688cf228a0cf6fb742e5
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-04-25 11:51:09 -0400

    modified publish_ to include more files and added runExample

--- R/publish.R
+++ R/publish.R
@@ -1,13 +1,31 @@
-publish_ <- function(htmlFile, ...){
+publish_ <- function(files, ...){
   UseMethod('publish_')
 }
 
-publish_.gist <- function(htmlFile, description, ...){
-  gist = create_gist(htmlFile, description = description, ...)
+publish_.gist <- function(files, description, ...){
+  gist = create_gist(files, description = description, ...)
   post_gist(gist)
 }
 
-publish_.rpubs <- function(htmlFile, description, ...){
+publish_.rpubs <- function(files, description, ...){
+  htmlFile = grep('.html$', files, value = T)
   url = markdown::rpubsUpload(title = description, htmlFile, ...)
   return(url)
+}
+
+#' Run an rCharts example from a github repo
+#' 
+# runExample(
+#    username = 'timelyportfolio', 
+#    repo = 'rCharts_polycharts_standalone', 
+#   filename = 'bar_athletes.R'
+# )
+runExample <- function(repo, username, ref = 'master', filename){
+  require(downloader)
+  tf <- tempfile(pattern = '*.R')
+  url <- sprintf('https://raw.github.com/%s/%s/%s/%s',
+    username, repo, ref, filename)
+  download(url, tf)
+  mychart = rCharts::create_chart(tf)
+  return(mychart)
 }
