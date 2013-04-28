53f8cda067b5e88b2f12229826697eecb5e84e1d
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-04-28 09:14:06 -0400

    big refactoring of rCharts Class
    - add LIB as field to support ext libraries
    - add srccode as code.R while publishing as gist

--- R/rChartsClass.R
+++ R/rChartsClass.R
@@ -1,7 +1,7 @@
-rCharts = setRefClass('rCharts', list(params = 'list', lib = 'character', 
+rCharts = setRefClass('rCharts', list(params = 'list', lib = 'character', LIB = 'list',
     srccode = 'ANY', tObj = 'list'), methods = list(
   initialize = function(){
-    srccode <<- NULL; tObj <<- list();
+    srccode <<- NULL; tObj <<- list(); LIB <<- list()
     params <<- list(dom = basename(tempfile('chart')),
       width = getOption('RCHART_WIDTH', 800), 
       height = getOption('RCHART_HEIGHT', 400)
@@ -11,15 +11,18 @@ rCharts = setRefClass('rCharts', list(params = 'list', lib = 'character',
     params <<- modifyList(params, list(...))
   },
   set = function(...){
+    if (length(LIB) == 0){
+      LIB <<- get_lib(lib)
+    }
     params <<- modifyList(params, list(...))
   },
   getPayload = function(chartId){
-    list(chartParams = toJSON(params), chartId = chartId, lib = lib)
+    list(chartParams = toJSON(params), chartId = chartId, lib = basename(lib))
   },
   html = function(chartId = NULL){
     if (!is.null(chartId)) params$dom <<- chartId else chartId <- params$dom
     params$id <<- params$dom
-    template = read_template(lib, 'layouts', 'chart.html')
+    template = read_file(file.path(LIB$url, 'layouts', 'chart.html'))
     html = render_template(template, getPayload(chartId))
     return(html)
   },
@@ -31,7 +34,7 @@ rCharts = setRefClass('rCharts', list(params = 'list', lib = 'character',
   },
   print = function(chartId = NULL){
     if (!is.null(chartId)) params$dom <<- chartId else chartId <- params$dom
-    chartDiv = sprintf("<div id='%s' class='rChart nvd3Plot %s'></div>", chartId, .self$lib)
+    chartDiv = sprintf("<div id='%s' class='rChart nvd3Plot %s'></div>", chartId, LIB$name)
     writeLines(c(chartDiv, .self$html(chartId)))
   },
   render = function(chartId = NULL, cdn = F){
@@ -39,11 +42,11 @@ rCharts = setRefClass('rCharts', list(params = 'list', lib = 'character',
     template = read_template(getOption('RCHART_TEMPLATE', 'rChart.html'))
     html = render_template(template, list(
       params = params,
-      assets = get_assets(lib, cdn),
+      assets = get_assets(LIB, static = T, cdn = cdn),
       chartId = chartId,
       script = .self$html(chartId),
       CODE = srccode,
-      lib = lib,
+      lib = LIB$name,
       tObj = tObj
     ))
   },
@@ -58,15 +61,22 @@ rCharts = setRefClass('rCharts', list(params = 'list', lib = 'character',
     } else {
       shiny_copy = .self$copy()
       shiny_copy$params$dom = 'show'
-      assign(".rChart_object", shiny_copy, envir = .GlobalEnv) 
+      assign(".rChart_object", shiny_copy, envir = .GlobalEnv)
       shiny::runApp(file.path(system.file(package = "rCharts"), "shiny"))
     }
   },
   publish = function(description = "", ..., host = 'gist'){
     htmlFile = file.path(tempdir(), 'index.html'); on.exit(unlink(htmlFile))
     .self$save(destfile = htmlFile, cdn = T)
-    class(htmlFile) = host
-    publish_(htmlFile = htmlFile, description = description, ...)
+    if (!is.null(.self$srccode)){
+      codeFile = file.path(tempdir(), 'code.R'); on.exit(unlink(htmlFile))
+      writeLines(.self$srccode, con = codeFile)
+      files = c(htmlFile, codeFile)
+    } else {
+      files = htmlFile
+    }
+    class(files) = host
+    publish_(files = files, description = description, ...)
   }
 ))
 
