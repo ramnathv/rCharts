7455da5b8d1a1f74b378d0c62be5e6740ba7d18c
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-05-10 16:55:38 -0400

    update examples

--- inst/libraries/leaflet/examples/example1.R
+++ inst/libraries/leaflet/examples/example1.R
@@ -3,3 +3,9 @@ map1 = Leaflet$new()
 map1$setView(45.5236, -122.675, 4)
 map1$tileLayer("http://a.tiles.mapbox.com/v3/mapbox.control-room/{z}/{x}/{y}.png", 8)
 map1
+
+
+map1b = Leaflet$new()
+map1b$setView(45.5236, -122.675, 4)
+map1b$tileLayer(provider = 'MapQuestOpen.OSM')
+map1b
--- inst/libraries/leaflet/examples/example4.R
+++ inst/libraries/leaflet/examples/example4.R
@@ -8,3 +8,16 @@ dat <- head(crime)[,c('lat', 'lon', 'offense')]
 names(dat) <- c('lat', 'lng', 'offense')
 map4$geocsv(dat)
 map4
+
+# {title: New Map} ----
+map5 = Leaflet$new()
+map5$setView(37.27667, -91.60611, 4)
+map5$tileLayer("http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png")
+
+dat <- read.csv('geoCoded.csv')
+names(dat) <- c('address', 'lat', 'lng')
+dat <- transform(dat, color = 'red', fillColor = '#f03', fillOpacity = 0.5, radius = 10)
+map5$circle(dat)
+map5
+
+
