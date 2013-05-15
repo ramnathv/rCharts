15d3e7ffc97f388917f5fbad4c72037d279ed4cd
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-05-14 22:40:04 -0400

    fix examples for leaflet and xcharts.

--- inst/libraries/leaflet/examples/example1.R
+++ inst/libraries/leaflet/examples/example1.R
@@ -1,7 +1,7 @@
 require(rCharts)
 map1 = Leaflet$new()
 map1$setView(45.5236, -122.675, 4)
-map1$tileLayer("http://a.tiles.mapbox.com/v3/mapbox.control-room/{z}/{x}/{y}.png", 8)
+map1$tileLayer("http://a.tiles.mapbox.com/v3/mapbox.control-room/{z}/{x}/{y}.png", zoom = 8)
 map1
 
 
