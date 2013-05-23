0ac2896acf5519daf858d26ba323456d1126706a
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-05-23 16:14:56 -0400

    add metadata to morrisjs examples

--- inst/libraries/morris/examples.R
+++ inst/libraries/morris/examples.R
@@ -1,21 +1,21 @@
 require(rCharts)
 haireye = as.data.frame(HairEyeColor)
 
-## Simple Bar Chart
+## {title: Simple Bar Chart}
 dat = subset(haireye, Sex == "Female" & Eye == "Blue")
 p1 <- mPlot(x = 'Hair', y = list('Freq'), data = dat, type = 'Bar', labels = list("Count"))
 p1
 
-### Check if hideHover = "auto" switches hover off when not on chart
+### {title: Simple Bar Chart, tag: "No Hover"}
 p1$set(hideHover = "auto")
 p1
 
-## Multi Bar Chart
+## {title: Multi Bar Chart}
 dat = subset(haireye, Sex == "Female")
 p2 <- mPlot(Freq ~ Eye, group = "Hair", data = dat, type = "Bar", labels = 1:4)
 p2
 
-## Line Chart
+## {title: Line Chart}
 data(economics, package = 'ggplot2')
 dat = transform(economics, date = as.character(date))
 p3 <- mPlot(x = "date", y = list("psavert", "uempmed"), data = dat, type = 'Line',
@@ -26,7 +26,7 @@ p3$set(xLabelFormat = "#! function (x) {
 p3
 
 
-## Area Chart
+## {title: Area Chart}
 p3$set(type = 'Area')
 p3
 
