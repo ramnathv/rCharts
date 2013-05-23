2906f9a3a0c260f2b1921055ea5c7b12c975397e
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-05-23 16:15:46 -0400

    add more examples for Leaflet

--- inst/libraries/leaflet/examples/example1.R
+++ inst/libraries/leaflet/examples/example1.R
@@ -1,11 +1,9 @@
 require(rCharts)
 map1 = Leaflet$new()
-map1$setView(45.5236, -122.675, 4)
+map1$setView(c(45.5236, -122.675), 13)
 map1$tileLayer("http://a.tiles.mapbox.com/v3/mapbox.control-room/{z}/{x}/{y}.png", zoom = 8)
 map1
 
-
-map1b = Leaflet$new()
-map1b$setView(45.5236, -122.675, 4)
-map1b$tileLayer(provider = 'MapQuestOpen.OSM')
-map1b
+map1 = Leaflet$new()
+map1$setView(c(45.50867, -73.55399), 13)
+map1
--- inst/libraries/leaflet/examples/example4.R
+++ inst/libraries/leaflet/examples/example4.R
@@ -1,6 +1,7 @@
 map4 = Leaflet$new()
-map4$setView(29.6779, -95.4379, 10)
+map4$setView(c(29.6779, -95.4379), 10)
 map4$tileLayer("http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png")
+# map4$tileLayer(provider = 'Stamen.Terrain')
 
 
 data(crime, package = 'ggmap')
