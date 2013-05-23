6d9a5f7152b88761926acef194f8cac6f19b9b96
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-05-23 16:16:39 -0400

    update Leaflet.R with new features.

--- R/Leaflet.R
+++ R/Leaflet.R
@@ -1,7 +1,14 @@
 Leaflet = setRefClass('Leaflet', contains = 'rCharts', methods = list(
-  setView = function(lat, long, zoom = 4, ...){
-    params$center <<- list(lat, long)
-    params$viewOpts <<- list(zoom, ...)
+  initialize = function(){
+    callSuper()
+    .self$tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png')
+    params$addons <<- list(enablePopover = FALSE)
+  },
+  enablePopover = function(e = TRUE){
+    params$addons$enablePopover <<- e
+  },
+  setView = function(center, zoom = 10, ...){
+    params <<- c(params, list(center = center, zoom = zoom))
   },
   tileLayer = function(urlTemplate, provider = NULL, ...){
     if (!is.null(provider)){
@@ -9,23 +16,29 @@ Leaflet = setRefClass('Leaflet', contains = 'rCharts', methods = list(
     } else {
       params$urlTemplate <<- urlTemplate
       params$layerOpts <<- list(..., 
-        attribution =  'Map data<a href="http://openstreetmap.org">OpenStreetMap</a>contributors, Imagery<a href="http://mapbox.com">MapBox</a>'
+        attribution =  'Map data<a href="http://openstreetmap.org">OpenStreetMap</a>
+         contributors, Imagery<a href="http://mapbox.com">MapBox</a>'
       )
     }
   },
-  marker = function(lat, long, ...){
+  marker = function(LatLng, ...){
     m = list(
-      marker = list(lat, long),
+      marker = as.list(LatLng),
       addTo = '#! map !#',
       ...
     )
-    if (length(params$marker) > 0){
-      params$marker <<- c(params$marker, list(m))
-    } else {
-      params$marker <<- list(m)
-    }
+    params$marker <<- c(params$marker, list(m))
   },
-  circle = function(circleData){
+  circle = function(LatLng, radius = 500, ...){
+    circle_ = list(
+      circle = LatLng,
+      setRadius = radius,
+      ...,
+      addTo = '#! map !#'
+    )
+    params$circle <<- c(params$circle, list(circle_))
+  },
+  circle2 = function(circleData){
     require(plyr)
     dat = alply(circleData, 1, function(c){
       list(
@@ -37,6 +50,7 @@ Leaflet = setRefClass('Leaflet', contains = 'rCharts', methods = list(
   },
   geocsv = function(data){
     paste2 = function(...) {paste(..., sep = ';')}
+    params$addons$geocsv <<- TRUE
     params$geocsv <<- list(
       titles = names(data),
       data = paste(do.call('paste2', data), collapse = '\n')
@@ -44,9 +58,22 @@ Leaflet = setRefClass('Leaflet', contains = 'rCharts', methods = list(
   },
   getPayload = function(chartId){
     marker = paste(lapply(params$marker, toChain, obj =  'L'), collapse = '\n')
-    chartParams = toJSON(params[!(names(params) %in% c('marker'))])
-    list(chartParams = chartParams, chartId = chartId, lib = basename(lib),
-      marker = marker
+    circle = paste(lapply(params$circle, toChain, obj =  'L'), collapse = '\n')
+    # circle = toChain(params$circle, obj = 'L')
+    chartParams = toJSON(params[!(names(params) %in% c('marker', 'circle', 'addons'))])
+    list(
+      chartParams = chartParams, 
+      chartId = chartId, 
+      lib = basename(lib),
+      marker = marker,
+      circle = circle,
+      addons = params$addons
     )
   }
 ))
+
+rqMap <- function(location = "montreal", ...){
+  myMap = Leaflet$new()
+  myMap$setView(c(LngLat$lat, LngLat$lon), ...)
+  return(myMap)
+}
