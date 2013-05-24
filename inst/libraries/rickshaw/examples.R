721016581e0326125ab8db3207948d73d011c3b6
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-05-24 16:20:15 -0400

    update rickshaw examples

--- inst/libraries/rickshaw/examples.R
+++ inst/libraries/rickshaw/examples.R
@@ -21,3 +21,37 @@ usp = reshape2::melt(USPersonalExpenditure)
 p4 <- Rickshaw$new()
 p4$layer(value ~ Var2, group = 'Var1', data = usp, type = 'area')
 p4$show(T)
+
+dat <- yaml::yaml.load('[ { x: 0, y: 40 }, { x: 1, y: 49 }, { x: 2, y: 17 }, { x: 3, y: 42 } ]')
+
+dat <- data.frame(
+  x = c(0, 1, 2, 3),
+  y = c(40, 49, 2, 17)
+)
+
+
+dat <- data.frame(
+  x = seq(1910, 2010, 10),
+  y = c(92228531, 106021568, 123202660, 132165129, 151325798, 179323175,
+    203211926, 226545805,  248709873,  281421906, 308745538)
+)
+
+dat <- USPop
+dat <- transform(dat, year = to_jsdate(as.Date(paste(year, '01', '01', sep = '-'))))
+p4 <- Rickshaw$new()
+p4$layer(population ~ year, data = dat, type = 'area', colors = 'steelblue')
+p4$yAxis(orientation = 'left')
+p4$set(width = 540, height = 240)
+options(RCHART_TEMPLATE = 'Rickshaw.html')
+p4$save('inst/libraries/rickshaw/test-rickshaw2.html')
+
+
+uspexp <- reshape2::melt(USPersonalExpenditure)
+names(uspexp) <- c('category', 'year', 'expenditure')
+uspexp <- transform(uspexp, year = to_jsdate(as.Date(paste(year, '01', '01', sep = '-'))))
+p4 <- Rickshaw$new()
+p4$layer(expenditure ~ year, group = 'category', data = uspexp, type = 'area')
+p4$yAxis(orientation = 'left')
+p4$xAxis(type = 'Time')
+p4$set(width = 540, height = 240)
+options(RCHART_TEMPLATE = 'Rickshaw.html')
