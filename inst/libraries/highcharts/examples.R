d3eea018dd9f50b6b7bf3855ad75269a449b68a8
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-06-07 13:14:46 +0200

    updated highcharts examples

--- inst/libraries/highcharts/examples.R
+++ inst/libraries/highcharts/examples.R
@@ -1,70 +1,24 @@
 library(rCharts)
 
-## Example 1
-
-# Prepare data
-x <- data.frame(USPersonalExpenditure)
-colnames(x) <- substr(colnames(x), 2, 5)
-
-# Create chart
-a <- rCharts:::Highcharts$new()
-a$chart(type = "line")
-a$title(text = "US Personal Expenditure")
-a$xAxis(categories = rownames(x))
-a$yAxis(title = list(text = "Billions of dollars"))
-a$data(x)
-a
-
-
-## Example 2
-# Prepare data
-x <- data.frame(USPersonalExpenditure)
-colnames(x) <- substr(colnames(x), 2, 5)
-
-# Create chart
-a <- rCharts:::Highcharts$new()
-a$chart(type = "bar")
-a$title(text = "US Personal Expenditure")
-a$xAxis(categories = rownames(x))
-a$yAxis(title = list(text = "Billions of dollars"))
-a$data(x)
-a
-
-## Example 3
-
-a1 <- rCharts:::Highcharts$new()
-a1$title(text = "Fruits")
-a1$data(x = c("Apples", "Bananas", "Oranges"), y = c(15, 20, 30), type = "pie", 
-        name = "Amount")
-a1
-
-## Example 4
-
-# Prepare data
-x <- data.frame(USPersonalExpenditure)
-colnames(x) <- substr(colnames(x), 2, 5)
-
-# Create chart
-a <- rCharts:::Highchart$new()
-a$chart(type = "column")
-a$title(text = "US Personal Expenditure")
-a$xAxis(categories = rownames(x))
-a$yAxis(title = list(text = "Billions of dollars"))
-a$data(x)
-a
-
-## Example 5
+# Example 1
 hPlot(x = "Height", y = "Pulse", data = MASS::survey, type = "scatter", group = "Exer")
 
-## Example 6
-
+## Example 2
 a <- hPlot(x = "Height", y = "Pulse", data = MASS::survey, type = "bubble", title = "Zoom demo", subtitle = "bubble chart", size = "Age", group = "Exer")
 a$chart(zoomType = "xy")
 a
 
-## Example 7
-hPlot(x = "Wr.Hnd", y = "NW.Hnd", data = MASS::survey, type = c("line", "bubble", "scatter"), group = "Clap", size = "Age")
-
-## Example 8
+## Example 4
 x <- data.frame(key = c("a", "b", "c"), value = c(1, 2, 3))
 hPlot(x = "key", y = "value", data = x, type = "pie")
+
+## Example 5
+a <- hPlot(x = 'Height', y = 'Pulse', data = MASS::survey, type = 'scatter', group = 'Sex', radius = 6)
+a$colors('rgba(223, 83, 83, .5)', 'rgba(119, 152, 191, .5)', 'rgba(60, 179, 113, .5)')
+a$legend(align = 'right', verticalAlign = 'top', layout = 'vertical')
+a$plotOptions(scatter = list(marker = list(symbol = 'circle')))
+a
+
+## Example 6
+a <- hPlot(x = 'Exer', y = 'N', data = freq, type = c('column', 'line'), group = 'M.I', radius = 6)
+a
