1a279202025844deb4afe611eb0e8e18c1988ab2
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-06-08 00:01:45 +0200

    update highcharts examples

--- inst/libraries/highcharts/examples.R
+++ inst/libraries/highcharts/examples.R
@@ -31,3 +31,49 @@ a <- hPlot(freq ~ Exer, data = plyr::count(MASS::survey, c('Sex', 'Exer')), type
 a$plotOptions(column = list(dataLabels = list(enabled = T, rotation = -90, align = 'right', color = '#FFFFFF', x = 4, y = 10, style = list(fontSize = '13px', fontFamily = 'Verdana, sans-serif'))))
 a$xAxis(labels = list(rotation = -45, align = 'right', style = list(fontSize = '13px', fontFamily = 'Verdana, sans-serif')), replace = F)
 a
+
+## Example 9 (not working properly)
+drill_function <- "#! function() {
+    var drilldown = this.drilldown;
+    function setChart(name, categories, data, color) {
+		chart.xAxis[0].setCategories(categories, false);
+		chart.series[0].remove(false);
+		chart.addSeries({
+			name: name,
+			data: data,
+			color: color || 'black'
+		}, false);
+		chart.redraw();
+    };
+    if (drilldown) { // drill down
+        setChart(drilldown.name, drilldown.categories, drilldown.data, drilldown.color);
+    } else { // restore
+        setChart(name, categories, data);
+    }
+} !#"
+
+a <- rCharts::Highcharts$new()
+a$chart(type = "column")
+a$series(data = list(
+    list(y = 15, drilldown = list(data = c(1, 2, 3))), 
+    list(y = 20, drilldown = list(data = c(1, 2, 3)))), name = "test")
+a$xAxis(categories = c("Brand A", "Brand B"))
+a$plotOptions(column = list(cursor = 'pointer', point = list(events = list(click = drill_function))))
+
+## Example 10
+a <- hPlot(freq ~ Exer, data = plyr::count(MASS::survey, c('Sex', 'Exer')), type = 'bar', group = 'Sex', group.na = 'NA\'s')
+a$plotOptions(bar = list(cursor = 'pointer', point = list(events = list(click = "#! function() { alert ('Category: '+ this.category +', value: '+ this.y); } !#"))))
+a
+
+## Example 11
+a <- rCharts::Highcharts$new()
+a$series(data = list(
+    list(y = 8, url = "https://github.com/metagraf/rHighcharts", color = "lightblue"),
+    list(y = 14, url = "https://github.com/metagraf/rVega", color = "lightpink"),
+    list(y = 71, url = "https://github.com/ramnathv/rCharts", color = "lightgreen")
+), type = "column", name = "Number of Stars")
+a$plotOptions(column = list(cursor = 'pointer', point = list(events = list(click = "#! function() { location.href = this.options.url; } !#"))))
+a$xAxis(categories = c("rHighcharts", "rVega", "rCharts"), title = list(text = ""))
+a$yAxis(title = list(text = ""))
+a$legend(enabled = F)
+a
