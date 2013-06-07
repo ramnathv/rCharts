4c306284665105e108f259b6cb761f34576a71c6
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-06-07 14:45:42 +0200

    update highcharts examples

--- inst/libraries/highcharts/examples.R
+++ inst/libraries/highcharts/examples.R
@@ -1,10 +1,10 @@
 library(rCharts)
 
 # Example 1
-hPlot(x = "Height", y = "Pulse", data = MASS::survey, type = "scatter", group = "Exer")
+hPlot(Pulse ~ Height, data = MASS::survey, type = "scatter", group = "Exer")
 
 ## Example 2
-a <- hPlot(x = "Height", y = "Pulse", data = MASS::survey, type = "bubble", title = "Zoom demo", subtitle = "bubble chart", size = "Age", group = "Exer")
+a <- hPlot(Pulse ~ Height, data = MASS::survey, type = "bubble", title = "Zoom demo", subtitle = "bubble chart", size = "Age", group = "Exer")
 a$chart(zoomType = "xy")
 a
 
@@ -13,12 +13,12 @@ x <- data.frame(key = c("a", "b", "c"), value = c(1, 2, 3))
 hPlot(x = "key", y = "value", data = x, type = "pie")
 
 ## Example 5
-a <- hPlot(x = 'Height', y = 'Pulse', data = MASS::survey, type = 'scatter', group = 'Sex', radius = 6)
+a <- hPlot(Pulse ~ Height, data = MASS::survey, type = 'scatter', group = 'Sex', radius = 6)
 a$colors('rgba(223, 83, 83, .5)', 'rgba(119, 152, 191, .5)', 'rgba(60, 179, 113, .5)')
 a$legend(align = 'right', verticalAlign = 'top', layout = 'vertical')
 a$plotOptions(scatter = list(marker = list(symbol = 'circle')))
 a
 
 ## Example 6
-a <- hPlot(x = 'Exer', y = 'N', data = freq, type = c('column', 'line'), group = 'M.I', radius = 6)
+a <- hPlot(N ~ Exer, data = freq, type = c('column', 'line'), group = 'M.I', radius = 6)
 a
