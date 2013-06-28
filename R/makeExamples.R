3851b23409abd4e67a342039df89349ff2151427
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-06-28 12:38:19 -0400

    add function to automate screenshots (draft)

--- R/makeExamples.R
+++ R/makeExamples.R
@@ -40,3 +40,26 @@ create_chart <- function(rFile){
   )
   return(chart)
 }
+
+
+#' Automate screenshot of an rChart, optionally upload it to imgur
+#'
+#' @param path to R file containing code to create an rChart
+#' @param imgname name of the plot to save to
+take_screenshot <- function(rFile, imgname = 'plot1', upload = F){
+  rCode = paste(readLines(rFile, warn = F), collapse = "\n")
+  script = system.file('utils', 'screenshot.js', package = 'rCharts')
+  chart = source(rFile, local = TRUE)$value
+  chart$set(width = 600, height = 325)
+  tf <- tempfile(fileext = ".html"); on.exit(unlink(tf))
+  chart$save(tf)
+  cmd1 <- sprintf('casperjs %s %s %s', script, tf, imgname)
+  system(cmd1)
+  # system(sprintf("convert %s.png  -resize 288x172", imgname))
+  if (upload){
+    h = knitr:::imgur_upload(paste0(imgname, '.png'))
+    return(h[1])
+  } else {
+    return(imgname)
+  }
+}
