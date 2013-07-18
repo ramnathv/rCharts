497953505e7465f1a8104b924cfaa03e6984ee0d
timelyportfolio <kent.russell@timelyportfolio.com>
2013-07-18 15:01:37 -0500

    complete very alpha version of datatables

---
+++ inst/libraries/datatables/examples.R
@@ -0,0 +1,31 @@
+require(rCharts)
+
+dTable <- Datatables()
+dTable$addTable(
+  iris,
+  sPaginationType= "full_numbers"
+)
+dTable
+
+dTable$addTable(
+  iris,
+  bScrollInfinite = T,
+  bScrollCollapse = T,
+  sScrollY = "200px"
+)
+dTable
+
+
+#not really a use case but a test to check for errors
+data(Orange)
+dTable$addTable(
+  Orange,
+  sScrollY = "200px",
+  bScrollCollapse = T,
+  bPaginate = F,
+  bJQueryUI = T,
+  aoColumnDefs = list(
+    sWidth = "5%", aTargets =  list(-1)
+  )
+)
+dTable
