804119e3cf9d5a10035d1ed8c992cfc74e1d93f3
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2013-12-13 14:18:57 -0500

    add htmlspecialchars function from fun package (thanks @yihui)

--- R/utils.R
+++ R/utils.R
@@ -183,6 +183,30 @@ render_template = function(template, data = parent.frame(1), ...){
   ), collapse = "\n")
 }
 
+##' Replace HTML special characters with HTML entities
+##'
+##' The characters \code{c("&", "\"", "'", "<", ">")} will be replaced
+##' with \code{c("&amp;", "&quot;", "&#039;", "&lt;", "&gt;")}
+##' respectively.
+##' @param string the string with (or w/o) HTML special chars
+##' @return the string with special chars replaced.
+##' @author Yihui Xie <\url{http://yihui.name}>
+##' @seealso \code{\link[base]{gsub}}
+##' @references \url{http://php.net/manual/en/function.htmlspecialchars.php}
+##' @keywords manip
+##' @export
+##' @examples
+##' htmlspecialchars("<a href = 'http://yihui.name'>Yihui</a>")
+##' # &lt;a href = &#039;http://yihui.name&#039;&gt;Yihui&lt;/a&gt;
+htmlspecialchars <- function(string) {
+  x = c("&", "\"", "'", "<", ">")
+  subx = c("&amp;", "&quot;", "&#039;", "&lt;", "&gt;")
+  for (i in seq_along(x)) {
+    string = gsub(x[i], subx[i], string, fixed = TRUE)
+  }
+  string
+}
+
 # tpl <- '{{# items }} {{{.}}}\n {{/ items}}'
 # items <- letters[1:5]
 # render_template(tpl)
