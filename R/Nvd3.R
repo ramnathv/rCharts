5084eea4458a34e170976c91137d4b8d947cecc0
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2014-01-06 17:40:01 -0500

    add preliminary support for subset filters using lodash.

--- R/Nvd3.R
+++ R/Nvd3.R
@@ -8,11 +8,23 @@ Nvd3 <- setRefClass('Nvd3', contains = 'rCharts', methods = list(
   initialize = function(){
     callSuper(); 
     params <<- c(params, list(controls = list(),
-      chart = list(), xAxis = list(), x2Axis = list(), yAxis = list()
+      chart = list(), xAxis = list(), x2Axis = list(), yAxis = list(),
+      filters = list()
     ))
   },
   chart = function(..., replace = F){
     params$chart <<- setSpec(params$chart, ..., replace = replace)
+  },
+  addFilters = function(...){
+    .self$setTemplate(
+      page = 'rChartControls2.html',
+      script = system.file('libraries', lib, 'controls', 
+        'script.html', package = 'rCharts')
+    )
+    .self$set(width = 700)
+    params$filters <<- toJSONArray2(ldply(list(...), function(y) {
+      data.frame(variable = y, value = unique(params$data[[y]]))
+    }), json = F)
   },
   xAxis = function(..., replace = F){
     params$xAxis <<- setSpec(params$xAxis, ..., replace = replace)
@@ -38,11 +50,13 @@ Nvd3 <- setRefClass('Nvd3', contains = 'rCharts', methods = list(
     x2Axis = toChain(params$x2Axis, 'chart.x2Axis')    
     yAxis = toChain(params$yAxis, 'chart.yAxis')
     controls_json = toJSON(params$controls)
+    filters_json = toJSON(params$filters)
     controls = setNames(params$controls, NULL)
-    opts = toJSON(params[!(names(params) %in% c('data', 'chart', 'xAxis', 'x2Axis', 'yAxis', 'controls'))])
+    opts = toJSON(params[!(names(params) %in% c('data', 'chart', 'xAxis', 'x2Axis', 'yAxis', 'controls', 'filters'))])
     list(opts = opts, xAxis = xAxis, x2Axis = x2Axis, yAxis = yAxis, data = data, 
          chart = chart, chartId = chartId, controls = controls, 
-         controls_json = controls_json, CODE = srccode
+         controls_json = controls_json, CODE = srccode, 
+         filters_json = filters_json, hasFilter = (length(params$filters) > 0)
     )
   }
 ))
